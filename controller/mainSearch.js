3d0bfdc41784383fe891b2a7ac36bf4b89fcc20a
Artiom <artiom.kolesnikov@gmail.com>
2016-01-04 08:50:36 -0300

    Agregada función para decodificar direcciones

--- controller/mainSearch.js
+++ controller/mainSearch.js
@@ -26,6 +26,32 @@ myapp.controller('mainSearchCtrl', ['$scope', 'uiGmapGoogleMapApi', '$geolocatio
 		};
 	});
 
+	// To be set by previous step
+	$scope.chosenTown = "Roma";
+
+	// geocode the given address
+	var geocodeAddress = function(address, callback) {
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode( { 'address': address}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				callback(results[0].geometry.location);
+			} else {
+				console.log("Geocode was not successful for the following reason: " + status);
+			}
+		});
+	};
+
+	// google maps is ready
+	uiGmapGoogleMapApi.then(function(maps) {
+		// geocode chosen town
+		geocodeAddress($scope.chosenTown, function(latLng){
+			console.log("1  " + latLng.lat());
+			console.log("2  " + latLng.lng());
+			//$scope.map = { center: { latitude: latLng.lat(), longitude: latLng.lng() }, zoom: 12, bounds: {}};
+		});
+
+	});
+
 	uiGmapGoogleMapApi.then(function(maps) {
 
 	});
