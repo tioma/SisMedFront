74fb669e34651eddd5b578b4e119584db6a88277
Artiom <artiom.kolesnikov@gmail.com>
2016-01-05 15:26:23 -0300

    Integración de facebook en la navegacion de la app

--- controller/uiNavigation.js
+++ controller/uiNavigation.js
@@ -6,21 +6,29 @@ myapp.controller('uiNavigationCtrl', ['$scope', '$facebook', function($scope, $f
 	$scope.isLoggedIn = false;
 	$scope.user = {};
 
+	$scope.logOut = function(){
+		$scope.isLoggedIn = false;
+	};
+
 	$facebook.getLoginStatus().then(function(result){
 		console.log(result);
-		if (result.status == 'connected'){
-			$facebook.api("/me?fields=name,email").then(
-				function(userData) {
-					$scope.user = userData;
-					console.log(userData);
-					//$scope.welcomeMsg = "Welcome " + response.name;
-					$scope.isLoggedIn = true;
-				},
-				function(err) {
-					console.log(err);
-					$scope.welcomeMsg = "Please log in";
-				});
-			console.log($scope.welcomeMsg);
+		if (result.status === 'connected'){
+			//conectado en fb y en la app
+			$facebook.api("/me?fields=name,email,picture").then(
+					function(userData) {
+						$scope.user = userData;
+						console.log(userData);
+						//$scope.welcomeMsg = "Welcome " + response.name;
+						$scope.isLoggedIn = true;
+					},
+					function(err) {
+						console.log(err);
+						//$scope.welcomeMsg = "Please log in";
+					});
+		} else if (result.status === 'not_authorized'){
+			//conectado en fb pero no en la app
+		} else { //status = 'unknown'
+			//no está conectado en fb así que no se sabe
 		}
 	});
 
